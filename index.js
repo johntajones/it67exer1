da1870047c3aaa9bfa71a083f9bccdf0d04992d0
John Tajones <johntajones@gmail.com>
2021-12-21 13:39:51 +0800

    Update index.js with additional routes

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+const { response } = require("express")
 const express = require("express")
 const app = express()
 const data = require("./Books.json")
 
 app.get("/api/books", (req, res) => {
-  res.send(data)
+  res.status(200).send(data)
 })
 
 app.get("/api/books/:id", (req, res) => {
   let result = data.find((book) => book.bookID === parseInt(req.params.id))
-  res.send(result)
+  if (!result) {
+    res.status(400).send("Book does not exist")
+    return
+  }
+
+  res.send({ request: "success", data: result })
 })
 
 app.listen(8000, () => console.log("Listening at port 8080"))
